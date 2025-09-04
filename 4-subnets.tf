b67218684e821055cce8756b29b401767be33e3a
UriahVictorious <uriahvictorious@gmail.com>
2025-09-04 06:40:50 -0400

    Update 4-subnets.tf

--- 4-subnets.tf
+++ 4-subnets.tf
@@ -1,18 +1,10 @@
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_subnetwork
 
-# Public Subnet
-resource "google_compute_subnetwork" "bam5-public" {
-  name          = "bam5-public"
-  ip_cidr_range = "10.235.0.0/24"
-  region        = "us-central1"
-  network       = google_compute_network.bam5-vpc.id
-}
-
-# Private Subnet
-resource "google_compute_subnetwork" "bam5-private" {
-  name                     = "bam5-private"
-  ip_cidr_range            = "10.236.0.0/24"
-  region                   = "southamerica-east1"
-  network                  = google_compute_network.bam5-vpc.id
+resource "google_compute_subnetwork" "uriah-subnet" {      # Update
+  name                     = "uriah-subnet"                # Update
+  ip_cidr_range            = "10.25.0.0/24"               # Update
+  region                   = "us-south1"         # Update
+  network                  = google_compute_network.uriah-vpc.id # Update
   private_ip_google_access = true
 }
+
